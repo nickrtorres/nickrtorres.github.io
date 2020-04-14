75ed0890ee2f3e40bdce0f8869bb88aa85825517
Nick Torres <nickrtorres@icloud.com>
2020-04-14 01:09:30 -0700

    title: revert addition of title plugin

--- _plugins/title.rb
+++
@@ -1,9 +0,0 @@
-# Lifted directly from http://paulherron.com/blog/custom_title_formatting_in_jekyll
-#
-# Use sentence case for titles
-class Jekyll::Post
-
-  def titleized_slug
-    self.slug.split(/[_-]/).join(' ').capitalize
-  end
-end
